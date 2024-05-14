7bc86191d649c6dcfa8804e49969e6ce761cd639
JoeyBol <jlwbol92@gmail.com>
2024-05-14 23:43:23 +0200

    Test enhanced for netlify

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	plugins: [enhancedImages(), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	resolve: {
+		alias: {
+			$img: mode === 'production' ? './static/img' : '../img'
+		}
 	}
-});
+}));
